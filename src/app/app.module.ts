92be55e4e060e52a3d659c5013be7a9506d71e8b
Luis Eduardo Castañeda Delgadillo <luis.castaneda@utags.edu.mx>
2020-03-10 15:32:18 -0600

    Eliminación de archivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-import { PagesComponent } from './home/pages/pages.component';
 import { FooterComponent } from './componets/footer/footer.component';
 import { HeaderComponent } from './componets/header/header.component';
-import { RegEmpComponent } from './reg-emp/reg-emp.component';
-import { VacanteFormComponent } from './vacante-form/vacante-form.component';
-import { ModelsComponent } from './models/models.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent,
-    PagesComponent,
     FooterComponent,
-    HeaderComponent,
-    RegEmpComponent,
-    FormVacanteComponent,
-    VacanteFormComponent,
-    ModelsComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
