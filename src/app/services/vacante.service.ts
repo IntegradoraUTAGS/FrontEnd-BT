3e198fb3b6d5e954f7a8f9db9890764008344c2c
Citlalli <takiita@hotmail.com>
2020-03-10 16:17:33 -0600

    formulario de vacantes con su service

--- src/app/services/vacante.service.ts
+++ src/app/services/vacante.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { vacantesModelo } from '../vacantes.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class VacanteService {
+export class vacantesService {
 
-  constructor() { }
+  url = `http://localhost:3000`;
+
+  constructor(private http: HttpClient) { }
+
+  registrarVacantes(vacantes: vacantesModelo) {
+    return this.http.post(`${this.url}/vacante/registrar`, vacantes).toPromise();
+  }
 }
