1cac201025efc28e2d00aa1353dd017ba3a9fa3b
DianaLoera <grisceldaloera@gmail.com>
2020-04-17 03:12:10 -0500

    validaciones de  los demas formularios

--- src/app/re/re.component.spec.ts
+++ src/app/re/re.component.spec.ts
@@ -1,6 +1,6 @@
+
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-<<<<<<< HEAD:src/app/re/re.component.spec.ts
 import { REComponent } from './re.component';
 
 describe('REComponent', () => {
@@ -10,27 +10,12 @@ describe('REComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ REComponent ]
-=======
-import { HomeComponent } from './home.component';
-
-describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
->>>>>>> 9cfe8d9bea822fb05c7283cf2094bbd8667580cb:src/app/home/home.component.spec.ts
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-<<<<<<< HEAD:src/app/re/re.component.spec.ts
     fixture = TestBed.createComponent(REComponent);
-=======
-    fixture = TestBed.createComponent(HomeComponent);
->>>>>>> 9cfe8d9bea822fb05c7283cf2094bbd8667580cb:src/app/home/home.component.spec.ts
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
